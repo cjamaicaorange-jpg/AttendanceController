74cdf78258afb0f530f577930d3d27c5f7ef9655
X1ron1X <cjamaicaorange@gmail.com>
2026-06-12 17:01:53 +0800

    Controller

---
+++ AttendanceControl/ApiattdnModels.cs
@@ -0,0 +1,9 @@
+﻿namespace AttendanceControl
+{
+    public class ApiattdnModels
+    {
+        public string StudentName { get; set; }
+        public string Day { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ AttendanceControl/AttendanceController.cs
@@ -0,0 +1,60 @@
+﻿using AttendanceManagementAppService;
+using AttendanceManagementModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttendanceControl.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AttendanceController : ControllerBase
+    {
+        private readonly AttendanceAppService _appService;
+
+        public AttendanceController()
+        {
+            _appService = new AttendanceAppService();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_appService.GetAttendance());
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] AttendanceItems item)
+        {
+            if (item == null)
+                return BadRequest();
+
+            _appService.AddRecord(
+                item.StudentName,
+                item.Day,
+                item.Status);
+
+            return Ok(item);
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] AttendanceItems item)
+        {
+            if (item == null)
+                return BadRequest();
+
+            _appService.UpdateRecord(
+                item.StudentName,
+                item.Day,
+                item.Status);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{studentName}")]
+        public IActionResult Delete(string studentName)
+        {
+            _appService.DeleteRecord(studentName);
+
+            return NoContent();
+        }
+    }
+}
